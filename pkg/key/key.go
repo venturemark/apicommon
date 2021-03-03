8dc14c65550f3862fdd2de3da4e55dc34bb08bc7
Tim Schindler <xh3b4sd@gmail.com>
2021-03-03 22:17:02 +0100

    update role key (#6)

--- pkg/key/key.go
+++ pkg/key/key.go
@@ -8,6 +8,20 @@ const (
 )
 
 const (
+	// Message is the storage key for mapping message IDs. An update can have
+	// multiple messages.
+	Message = "org:%s:tml:%s:upd:%s:mes"
+)
+
+const (
+	// Role is the storage key for mapping role IDs. An user can have multiple
+	// roles. The inserted resource key is the filled, hashed version of any of
+	// the other resource keys defined here, e.g. sha265("org:al9qy:tml") for
+	// records of timeline roles.
+	Role = "res:%s:rol"
+)
+
+const (
 	// Timeline is the storage key for mapping timeline IDs. An organization can
 	// own multiple timelines.
 	Timeline = "org:%s:tml"
@@ -17,16 +31,4 @@ const (
 	// Update is the storage key for mapping update IDs. An audience can have
 	// multiple updates.
 	Update = "org:%s:tml:%s:upd"
-)
-
-const (
-	// Message is the storage key for mapping message IDs. An update can have
-	// multiple messages.
-	Message = "org:%s:tml:%s:upd:%s:mes"
-)
-
-const (
-	// Role is the storage key for mapping role IDs. An user can have multiple
-	// roles.
-	Role = "usr:%s:res:rol"
 )
