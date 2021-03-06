b82f9a1f9219e6cd8ce138bcb8c9f5462065ed99
Tim Schindler <xh3b4sd@gmail.com>
2021-03-06 23:16:49 +0100

    fix hash (#15)

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -14,10 +14,15 @@ func Audience(m map[string]string) string {
 		aui = m[metadata.AudienceID]
 	}
 
-	return hash(fmt.Sprintf(key.Audience, aui))
+	return hash(key.Audience, aui)
 }
 
 func Message(m map[string]string) string {
+	var mei string
+	{
+		mei = m[metadata.MessageID]
+	}
+
 	var tii string
 	{
 		tii = m[metadata.TimelineID]
@@ -33,19 +38,10 @@ func Message(m map[string]string) string {
 		vei = m[metadata.VentureID]
 	}
 
-	return hash(fmt.Sprintf(key.Message, vei, tii, upi))
+	return hash(key.Message, vei, tii, upi, mei)
 }
 
 func Timeline(m map[string]string) string {
-	var vei string
-	{
-		vei = m[metadata.VentureID]
-	}
-
-	return hash(fmt.Sprintf(key.Timeline, vei))
-}
-
-func Update(m map[string]string) string {
 	var tii string
 	{
 		tii = m[metadata.TimelineID]
@@ -56,7 +52,26 @@ func Update(m map[string]string) string {
 		vei = m[metadata.VentureID]
 	}
 
-	return hash(fmt.Sprintf(key.Update, vei, tii))
+	return hash(key.Timeline, vei, tii)
+}
+
+func Update(m map[string]string) string {
+	var tii string
+	{
+		tii = m[metadata.TimelineID]
+	}
+
+	var upi string
+	{
+		upi = m[metadata.UpdateID]
+	}
+
+	var vei string
+	{
+		vei = m[metadata.VentureID]
+	}
+
+	return hash(key.Update, vei, tii, upi)
 }
 
 func Venture(m map[string]string) string {
@@ -65,13 +80,13 @@ func Venture(m map[string]string) string {
 		vei = m[metadata.VentureID]
 	}
 
-	return hash(fmt.Sprintf(key.Venture, vei))
+	return hash(key.Venture, vei)
 }
 
-func hash(s string) string {
+func hash(f string, v ...interface{}) string {
 	h := sha256.New()
 
-	_, err := h.Write([]byte(s))
+	_, err := h.Write([]byte(fmt.Sprintf(f+":%s", v...)))
 	if err != nil {
 		panic(err)
 	}
