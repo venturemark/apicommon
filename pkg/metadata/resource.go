304d5cc7942584e3b9c76c9cdaf562959b528904
Tim Schindler <xh3b4sd@gmail.com>
2021-03-08 18:37:48 +0100

    add metadata for resource visibility (#16)

--- pkg/metadata/resource.go
+++ pkg/metadata/resource.go
@@ -1,6 +1,7 @@
 package metadata
 
 const (
-	ResourceID   = "resource.venturemark.co/id"
-	ResourceKind = "resource.venturemark.co/kind"
+	ResourceID         = "resource.venturemark.co/id"
+	ResourceKind       = "resource.venturemark.co/kind"
+	ResourceVisibility = "resource.venturemark.co/visibility"
 )
