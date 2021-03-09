000302e06c12738ea603011fe06490bd992a9820
Tim Schindler <xh3b4sd@gmail.com>
2021-03-09 21:43:43 +0100

    refactor key management

--- pkg/metadata/resource.go
+++ pkg/metadata/resource.go
@@ -1,7 +1,6 @@
 package metadata
 
 const (
-	ResourceHash       = "resource.venturemark.co/hash"
 	ResourceKind       = "resource.venturemark.co/kind"
 	ResourceVisibility = "resource.venturemark.co/visibility"
 )
