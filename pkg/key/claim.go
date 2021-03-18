599ba19f129b1a3335cedb64f5ed402d032686b6
Tim Schindler <xh3b4sd@gmail.com>
2021-03-18 14:41:16 +0100

    add claim key

---
+++ pkg/key/claim.go
@@ -0,0 +1,45 @@
+package key
+
+import (
+	"fmt"
+
+	"github.com/venturemark/apicommon/pkg/metadata"
+)
+
+// Claim is the storage key for mapping user sibject claims to IDs. A user can
+// have multiple subject claims.
+func Claim(m map[string]string) *Key {
+	var cli string
+	{
+		cli = m[metadata.ClaimID]
+	}
+
+	var k *Key
+	{
+		var id *ID
+		{
+			id = &ID{
+				f: 0,
+				s: cli,
+			}
+		}
+
+		var ele string
+		{
+			ele = fmt.Sprintf("res:%s:cla", hash("cla:%s", cli))
+		}
+
+		var lis string
+		{
+			lis = fmt.Sprintf("res:%s:cla", hash("cla"))
+		}
+
+		k = &Key{
+			id:  id,
+			ele: ele,
+			lis: lis,
+		}
+	}
+
+	return k
+}
