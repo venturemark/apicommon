b0072a70d39e7cf50e7ecce119726c756e620137
Tim Schindler <xh3b4sd@gmail.com>
2021-03-19 13:58:54 +0100

    debug

--- pkg/key/hash.go
+++ pkg/key/hash.go
@@ -1,18 +1,17 @@
 package key
 
 import (
+	"crypto/sha256"
 	"fmt"
 )
 
 func hash(f string, v ...interface{}) string {
-	return fmt.Sprintf(f, v...)
+	h := sha256.New()
 
-	// h := sha256.New()
+	_, err := h.Write([]byte(fmt.Sprintf(f, v...)))
+	if err != nil {
+		panic(err)
+	}
 
-	// _, err := h.Write([]byte(fmt.Sprintf(f, v...)))
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// return fmt.Sprintf("%x", h.Sum(nil))
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
