6c04cc64719b8bbaa3ce618ee885edc98d206cd2
xh3b4sd <xh3b4sd@gmail.com>
2021-05-19 14:00:12 +0200

    add audience to task metadata (#39)

--- pkg/metadata/task.go
+++ pkg/metadata/task.go
@@ -2,6 +2,7 @@ package metadata
 
 const (
 	TaskAction   = "task.venturemark.co/action"
+	TaskAudience = "task.venturemark.co/audience"
 	TaskInterval = "task.venturemark.co/interval"
 	TaskResource = "task.venturemark.co/resource"
 )
